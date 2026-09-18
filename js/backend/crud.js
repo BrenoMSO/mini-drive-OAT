087ec19269207be7e3e899a3df42241b2131cbfa
Breno M. <breninhomc098@gmail.com>
2026-09-18 16:19:54 -0300

    Criação das funções de crud

---
+++ js/backend/crud.js
@@ -0,0 +1,39 @@
+let myArchives = [] || JSON.parse(localStorage.getItem("miniDriveArquivos"));
+var idNovo = 1;
+function SaveLocalStorage() {
+    localStorage.setItem("miniDriveArquivos", JSON.stringify(myArchives));
+}
+
+function CriarArquivo(nome, tamanho) {
+    const novoArquivo = {
+        id: idNovo,
+        nome: nome,
+        tamanho: tamanho
+    }
+    myArchives.push(novoArquivo);
+    SaveLocalStorage();
+    idNovo
+}
+
+function LerArquivo() {
+    return myArchives;
+}
+
+function AttArquivo(id, novoNome) {
+    let index = myArchives.findIndex(function(ficheiro) {
+        return ficheiro.id === id;
+    });
+    if (index !== -1) {
+        myArchives[index].nome = novoNome;
+
+        SaveLocalStorage();
+    }
+}
+
+function deletarArquivo(id) {
+    myArchives = myArchives.filter(function(ficheiro) {
+        return ficheiro.id !== id;
+    });
+
+    SaveLocalStorage();
+}
---
+++ js/frontend/app.js
@@ -0,0 +1,72 @@
+let fileNameInput = document.getElementById("fileName");
+let fileSizeInput = document.getElementById("fileSize");
+let uploadBtn = document.getElementById("uploadBtn");
+let archivesDiv = document.getElementById("savedArchives");
+
+
+
+function AtualizarInterface() {
+    archivesDiv.innerHTML = "";
+
+    let ficheiros = LerArquivo();
+
+    ficheiros.forEach(function(ficheiro) {
+        let itemDiv = document.createElement("div");
+
+        itemDiv.style.backgroundColor = "#24252a"; // Uma cor um pouco mais escura para o bloco
+        itemDiv.style.border = "2px solid #2dabf9";
+        itemDiv.style.padding = "20px";
+        itemDiv.style.borderRadius = "15px";
+        itemDiv.style.color = "white";
+        itemDiv.style.width = "200px"; 
+        itemDiv.style.display = "flex";
+        itemDiv.style.flexDirection = "column"; 
+        itemDiv.style.justifyContent = "space-between"; 
+        itemDiv.style.alignItems = "center"; 
+        itemDiv.style.gap = "15px"; 
+
+       
+        itemDiv.innerHTML = `
+            <div style="text-align: center; width: 100%; word-wrap: break-word;">
+                <div style="font-size: 50px; margin-bottom: 10px;">📁</div>
+                <div><strong>${ficheiro.nome}</strong></div>
+                <div style="font-size: 13px; color: #aaa; margin-top: 5px;">${ficheiro.tamanho}</div>
+            </div>
+            
+            <div style="display: flex; gap: 10px; width: 100%;">
+            <button onclick="${prepararEdicao(ficheiro.id)}" style="background-color: #f39c12; color: white; border: none; padding: 8px; border-radius: 5px; cursor: pointer; flex: 1;">Editar</button>
+                <button onclick="console.log('Deletar ID: ${ficheiro.id}')" style="background-color: #e74c3c; color: white; border: none; padding: 8px; border-radius: 5px; cursor: pointer; flex: 1;">Excluir</button>
+            </div>
+        `;
+
+        archivesDiv.appendChild(itemDiv);
+    });
+}
+
+function prepararEdicao(id) {
+    let novoNome = prompt("Digite o nome novo para o arquivo");
+
+    if (novoNome !== null && novoNome.trim() !== "") {
+        AttArquivo(id, novoNome);
+    }
+    AtualizarInterface();
+}
+
+
+uploadBtn.addEventListener("click", function() {
+    let nome = fileNameInput.value;
+    let tamanho = fileSizeInput.value;
+
+    if (nome == "" || tamanho == "") {
+        alert("insira alguma informação");
+        return;
+    }
+
+    CriarArquivo(nome, tamanho);
+    fileNameInput = "";
+    fileSizeInput = "";
+
+    AtualizarInterface();
+
+});
+
